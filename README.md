9ac6fb45f6d827f0dcdd8f91d14d8793a3f1be0b
Alex Orrantia <alejandro.orrantia@gmail.com>
2020-09-02 18:10:27 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Playing around with a Laravel and Vue JS project.
+Building a dashboard for a Fictitious paper company.
---
+++ resources/js/components/users.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="flex flex-col break-words bg-white border border-2 rounded shadow-md">
+
+                <div class="font-semibold bg-gray-200 text-gray-700 py-3 px-6 mb-0">
+                    Title
+                </div>
+
+                <div class="w-full p-6">
+
+                </div>
+            </div>
+</template>
+
+<script>
+export default {
+
+    data: function(){
+
+        return {
+
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
